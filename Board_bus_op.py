632785bcffe5cedc4d1c2fe3d30d37f45987ea07
Gautam Sukumar <gautamsukumar@users.noreply.github.com>
2016-02-01 21:45:10 -0500

    Create Board_bus_op.py

---
+++ Board_bus_op.py
@@ -0,0 +1,26 @@
+============ RESTART: C:\ccmsuite\ccmsuite-master\Boarding_bus.py ============
+   0.000 agent.production_threshold None
+   0.000 agent.production_time_sd None
+   0.000 agent.production_match_delay 0
+   0.000 agent.production_time 0.05
+   0.000 agent.focus.chunk None
+   0.000 agent.focus.chunk goal:boardbus object:busstop
+   0.000 agent.production correct_bus
+   0.050 agent.production None
+I see the route 8 bus
+   0.050 agent.focus.chunk goal:boardbus object:stop_at_flag
+   0.050 agent.production stop_at_flag
+   0.100 agent.production None
+I am standing at the flag
+   0.100 agent.focus.chunk goal:boardbus object:open_door
+   0.100 agent.production open_door
+   0.150 agent.production None
+I am waiting for the door to open
+   0.150 agent.focus.chunk goal:boardbus object:climb_in
+   0.150 agent.production climb_in
+   0.200 agent.production None
+I have climbed in
+I have boarded the bus
+   0.200 agent.focus.chunk goal:stop
+   0.200 agent.production stop_production
+   0.250 agent.production None
